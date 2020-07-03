fa53ff53d7552af0012c0cf1523028757becf7ff
Robert Connot <rmconnot@Roberts-MacBook-Pro.local>
2020-07-03 14:27:11 -0600

    file name changes

---
+++ cut_descriptions.js
@@ -0,0 +1,15 @@
+const loinInfoText = "The area below the backbone is home to some of the most tender and popular cuts of beef, such as the Tenderloin, Strip Steak, T-Bone and Porterhouse Steaks. Loin cuts are great prepared on the grill or under a broiler.";
+
+const ribInfoText = "Situated under the front section of the backbone and used primarily for support. Popular cuts from the Rib include the rich, flavorful Ribeye Steak and the Prime Rib Roast.";
+
+const chuckInfoText = "This large primal comes from the shoulder area and yields cuts known for their rich, beefy flavor. Features roasts ideal for slow-cooking as well as more tender, grill-ready cuts such as the Flat Iron Steak.";
+
+const plateInfoText = "The section right under the rib primal with a higher fat content. The Short Plate is a source of Short Ribs and Ground Beef. It's also the home of the Skirt Steak—a thin, flavorful cut that's best when marinated and seared over high heat.";
+
+const flankInfoText = "Located below the Loin and home to the Flank Steak. Cuts from this area are lean, very flavorful, and best when braised, or marinated and grilled.";
+
+const brisketInfoText = "The animal's breast; it can be tougher because it is used for movement. An ideal cut for cooking low and slow-on the barbecue or in a slow-cooker.";
+
+const roundInfoText = "Home to lean, inexpensive cuts that come from the rump and hind legs. The muscles in this area are used for movement, so the beef is leaner and less tender. Often sold as roasts, steaks for marinating or Ground Beef.";
+
+const shankInfoText = "A portion of the leg, which is used extensively for movement. As a result, it is typically cut into cross sections called Shank Cross-Cuts which are braised to make flavorful, fork-tender dishes such as Osso Buco.";
